36e0406b0e9e38ba9a1d344f371188f910fcdd53
RamyPoe <coolshifter99@gmail.com>
2024-05-29 15:38:46 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,82 @@
+import java.util.Arrays;
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+    public static void main(String[] args) {
+
+
+        // double[] c1 = {6, -9, 5};
+        // int[] e1 = {0, 2, 3};
+        // // Polynomial p1 = new Polynomial(c1, e1);
+        // Polynomial p1 = new Polynomial("-54.0x^4+81.0x^6-45.0x^7");
         
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        
+        // double[] c2 = {-2, 9, -9};
+        // int[] e2 = {1, 2, 4};
+        // // Polynomial p2 = new Polynomial(c2, e2);
+        // // Polynomial p2 = new Polynomial("-12.0x^1+54.0x^2+18.0x^3-91.0x^4+45.0x^5");
+
+        // System.out.println(p1);
+        // System.out.println(p2); 
+        // System.out.println();
+
+        // Polynomial m1 = p2.add(p1);
+        // // Polynomial m2 = p1.multiply(p2);
+
+        // System.out.println();
+        // System.out.println(m1);
+        // // System.out.println(m2);
+
+
+
+        
+        System.out.println("\n=====================================\n");
+        
+
+        
+        double[] c1 = {6, -9, 5};
+        int[] e1 = {0, 2, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        System.out.println("[P1] = " + p1);
+        
+        Polynomial p2 = new Polynomial("-9x^4-2x^1+9x^2");
+        System.out.println("[P2] = " + p2);
+
+
+
+        Polynomial s = p2.add(p1);
+        System.out.println("[S] = [P1] + [P2] = " + s + "\n");
+
+        System.out.println("S(0.1) = " + s.evaluate(0.1));
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+        
+
+        System.out.println("\n=====================================\n");
+        
+
+        Polynomial m = p2.multiply(p1);
+        System.out.println("[M] = [P1] * [P2] = " + m + "\n");
+        
+        System.out.println("P(0.1) = " + m.evaluate(0.1));
+        if(m.hasRoot(-1))
+            System.out.println("-1 is a root of s");
+        else
+        System.out.println("-1 is not a root of s");
+
+        System.out.println("\n=====================================\n");
+        
+        p1 = new Polynomial("2-1x^2");
+        p2 = new Polynomial("2+1x^2");
+        m = p1.multiply(p2);
+        
+        System.out.println("[P1] = " + p1);
+        System.out.println("[P2] = " + p2);
+        System.out.println();
+        System.out.println("[M] = [P1] * [P2] = " + m);
+        
+
+        System.out.println("\n=====================================\n");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,284 @@
+import java.io.*;
+
 public class Polynomial {
+    // For double comparison
     private static final double ERR = 0.00001;
 
-    double[] coeff;
+    // Non-zero coeff
+    private double[] coeff;
 
+    // Corresponding to coeff, should be sorted ascending
+    private int[] exponents;
+
+    // Zero polynomial
     public Polynomial() {
-        coeff = new double[]{0};
+        coeff = null;
+        exponents = null;
     }
 
-    public Polynomial(double[] coeff) {
+    public Polynomial(double[] coeff, int[] exponents) {
+        // Backup in case of bad input
+        this();
+
+        // Make sure the lengths are matching
+        if (exponents.length == 0 || coeff.length != exponents.length) { return; }
+
+        // Internal structure will use sorted terms
+        sortTerms(coeff, exponents);
         this.coeff = coeff;
+        this.exponents = exponents;
+    } 
+
+    // Reading polynomial from file
+    public Polynomial(File f) throws IOException {
+        this();
+
+        // Read the line from the file
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine().strip();
+        fr.close();
+
+        // Set the arrays based on the string
+        setPolynomialFromString(line);
     }
+
+    // Setting the polynomial from a string
+    public Polynomial(String line) { this(); setPolynomialFromString(line); }
+
+    // Takes string and assigns arrays based on parsing, used in constructor
+    private void setPolynomialFromString(String line) {
+
+        // Positive lookahead to keep the sign of term
+        String[] terms = line.split("((?=[+-]))");
+
+        double[] co = new double[terms.length];
+        int[] ex = new int[terms.length];
+
+        // Save coefficient and expoonent for each term
+        for (int i = 0; i < terms.length; i++) {
+            String[] term = terms[i].split("x\\^");
+            co[i] = Double.parseDouble(term[0]);
+            if (term.length > 1) { ex[i] = Integer.parseInt(term[1]); } else { ex[i] = 0; }
+        }
+
+        // Underlying structure assumes it is sorted
+        sortTerms(co, ex);
+
+        // Save the terms
+        this.coeff = co;
+        this.exponents = ex;
+    }
+
+    public void saveToFile(String filename) throws IOException {
+
+        // Generate string from the polynomial
+        FileWriter fw = new FileWriter(filename);
+        fw.write(this.toString());
+        fw.close();
+    }
+
     
     public Polynomial add(Polynomial p) {
-        int max_deg = p.coeff.length > coeff.length ? p.coeff.length : coeff.length;
+        // Edge case
+        if (coeff == null) { return p.copy(); }
+        if (p.coeff == null) { return this.copy(); }
+        
+        // Subtract common exponents to find length of sum
+        int poly_sum_length = exponents.length + p.exponents.length;
+        int i1 = 0; int i2 = 0;
+        while (i1 < exponents.length && i2 < p.exponents.length) {
+            if (exponents[i1] == p.exponents[i2]) {
+                poly_sum_length--;
+                
+                // The terms cancel out
+                if (doubleEquals(coeff[i1], -p.coeff[i2])) { poly_sum_length--; }
 
-        double[] nCoeff = new double[max_deg];
-        for (int i = 0; i < max_deg; i++) {
-            if (i < p.coeff.length) { nCoeff[i] += p.coeff[i]; };
-            if (i < coeff.length) { nCoeff[i] += coeff[i]; };
+                i1++; i2++;
+                continue;
+            }
+            if (exponents[i1] > p.exponents[i2]) { i2++; } else { i1++; }
         }
-        return new Polynomial(nCoeff);
+        
+        // Arrays for the new polynomial
+        double[] nCoeff = new double[poly_sum_length];
+        int[] nExponents = new int[poly_sum_length];
+        
+        // Add the terms
+        i1 = 0; i2 = 0;
+        for (int i = 0; i < poly_sum_length; i++) {
+
+            // Have exhausted first polynomial or ( none are exhausted and second polynomial has smaller exponent )
+            if (i1 >= exponents.length || (i2 < p.exponents.length && exponents[i1] > p.exponents[i2])) {
+                nCoeff[i] += p.coeff[i2];
+                nExponents[i] = p.exponents[i2];
+                i2++; continue;
+            }
+            
+            // Have exhausted second polynomial or ( none are exhausted and first polynomial has smaller exponent )
+            if (i2 >= p.exponents.length || (i1 < exponents.length && exponents[i1] < p.exponents[i2])) {
+                nCoeff[i] += coeff[i1];
+                nExponents[i] = exponents[i1];
+                i1++; continue;
+            }
+
+            // Equal exponents, add both coefficients
+            if (exponents[i1] == p.exponents[i2]) {
+                // Terms cancel out, don't count as an iteration
+                if (doubleEquals(coeff[i1], -p.coeff[i2])) { i--; i1++; i2++; continue; }
+
+                nCoeff[i] += coeff[i1] + p.coeff[i2];
+                nExponents[i] = exponents[i1];
+                i1++; i2++;
+            }
+
+        }
+
+        // Return the new polynomial
+        return new Polynomial(nCoeff, nExponents);
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        // Edge case
+        if (coeff == null || p.coeff == null) { return new Polynomial(); }
+
+        // Polynomial accumulator, start with zero polynomial
+        Polynomial accPolynomial = new Polynomial();
+
+        // For every term in this polynomial
+        for (int i = 0; i < coeff.length; i++) {
+            Polynomial tmp = p.copy();
+            
+            // Multiply first term into polynomial
+            for (int j = 0; j < tmp.coeff.length; j++) {
+                tmp.coeff[j] *= coeff[i];
+                tmp.exponents[j] += exponents[i];
+            }
+
+            // Add to the last product
+            accPolynomial = accPolynomial.add(tmp);
+        }
+
+        // Return the repeated sum
+        return accPolynomial;
+
+        /*
+
+        // Number of individual terms to add is the product of their lengths
+        int max_length = exponents.length*p.exponents.length;
+
+        // Exponents to work with in unsimplified form
+        double[] big_coeff = new double[max_length];
+        int[] big_exponents = new int[max_length];
+
+        // Perform crude multiplication
+        for (int i = 0; i < exponents.length; i++) {
+            for (int j = 0; j < p.exponents.length; j++) {
+                int index = i*p.exponents.length + j;
+
+                big_exponents[index] = exponents[i] + p.exponents[j];
+                big_coeff[index] = coeff[i] * p.coeff[j];
+            }
+        }
+
+        // Sort the terms by ascending exponent
+        sortTerms(big_coeff, big_exponents);
+
+        // Find what length should be for new arrays (number of unique exponents)
+        int prevExponent = -1; int uniqueCount = 0;
+        for (int i = 0; i < big_exponents.length; i++) {
+            if (big_exponents[i] != prevExponent) { uniqueCount++; prevExponent = big_exponents[i]; }
+        }
+
+        // Final arrays for polynomial
+        double[] nCoeff = new double[uniqueCount];
+        int[] nExponents = new int[uniqueCount];
+        
+        // Collapse the terms to remove duplicates
+        prevExponent = -1; int index = -1;
+        for (int i = 0; i < big_exponents.length; i++) {
+            if (big_exponents[i] != prevExponent) {
+                index++;
+                nExponents[index] = big_exponents[i];
+                nCoeff[index] = big_coeff[i];
+                prevExponent = big_exponents[i];
+            } else {
+                nCoeff[index] += big_coeff[i];
+            }
+        }
+
+        // Return the new polynomial
+        return new Polynomial(nCoeff, nExponents);
+
+        */
     }
 
     public double evaluate(double x) {
+        if (coeff == null) { return 0; }
+
         double result = 0;
         for (int i = 0; i < coeff.length; i++) {
-            result += coeff[i] * pow(x, i);
-            // System.out.println(coeff[i] + " * " + pow(coeff[i], i) + " = " + coeff[i] * pow(coeff[i], i));
+            result += coeff[i] * pow(x, exponents[i]);
         }
         return result;
     }
 
-    private double pow(double b, int e) {
+    // Returns copy that uses new arrays
+    private Polynomial copy() {
+        double[] cArr = new double[coeff.length];
+        for (int i = 0; i < cArr.length; i++) { cArr[i] = coeff[i]; }
+        int[] eArr = new int[exponents.length];
+        for (int i = 0; i < eArr.length; i++) { eArr[i] = exponents[i]; }
+        return new Polynomial(cArr, eArr);
+    }
+
+    // Given the two arrays it will sort them by increasing exponent while keeping correspondence
+    private void sortTerms(double[] cArr, int[] eArr) {
+        boolean sorted = false;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < eArr.length-1; i++) {
+                if (eArr[i] > eArr[i+1]) {
+                    swapArr(cArr, i, i+1);
+                    swapArr(eArr, i, i+1);
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    private static void swapArr(int[] arr, int i1, int i2)    { int t = arr[i1]; arr[i1] = arr[i2]; arr[i2] = t; }
+    private static void swapArr(double[] arr, int i1, int i2) { double t = arr[i1]; arr[i1] = arr[i2]; arr[i2] = t; }
+
+    private static double pow(double b, int e) {
         double result = 1;
         for (int i = 0; i < e; i++) { result *= b; }
         return result;
     }
 
-    public boolean hasRoot(int x) {
-        double num = evaluate(x);
-        return -ERR < num && num < ERR;
+    private static boolean doubleEquals(double a, double b) {
+        return Math.abs(a-b) <= ERR;
     }
+
+    public boolean hasRoot(double x) {
+        return doubleEquals(evaluate(x), 0d);
+    }
+
+    @Override
+    public String toString() {
+        if (coeff == null) { return ""; }
+
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < exponents.length; i++) {
+            if (i != 0 && coeff[i] > 0) out.append("+");
+            out.append(String.valueOf(coeff[i]));
+
+            if (exponents[i] == 0) continue;
+            out.append("x^");
+            out.append(String.valueOf(exponents[i]));
+        }
+        return out.toString();
+    }
+
+
 }
